6da10d88fd267e0481a29cb6423b0242fbf75423
giacomo9999 <gmarchesi888@gmail.com>
2024-01-04 16:38:45 -0500

    Implemented state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,45 @@
 import Book from "./components/Book.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type BookGoal = { title: string; description: string; id: number };
 
 export default function App() {
+  const [books, addBook] = useState<BookGoal[]>([]);
+
+  function handleAddBook() {
+    addBook((prevBooks) => {
+      const newBook: BookGoal = {
+        title: "Critique Of Pure Reason",
+        description: "a book about philosophy",
+        id: Math.random(),
+      };
+      return [...prevBooks, newBook];
+    });
+  }
+
   return (
     <div className="container-outer">
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>My Reading Goals</h1>
       </Header>
+      <button onClick={handleAddBook}>Add Goal</button>
 
-      <h1>Books Of Jim</h1>
-      <Book title="The Dying Earth">
-        <p>Blah blah blah</p>
-      </Book>
+      <h1>Books To Read</h1>
+      <ul>
+        {books.map((book) => {
+          return (
+            <li key={book.id}>
+              <Book
+                title={book.title}
+                description={book.description}
+                id={book.id}
+              ></Book>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,16 +1,19 @@
 import { type PropsWithChildren } from "react";
 
-type BookProps = PropsWithChildren<{ title: string }>;
+type BookProps = PropsWithChildren<{
+  title: string;
+  description: string;
+  id: number;
+}>;
 
-export default function Book({ title, children }: BookProps) {
+export default function Book({ title, description }: BookProps) {
   return (
     <>
       <div className="container-inner">
         <h2>{title}</h2>
-        <p>---</p>
-        {children}
+        <h3>{description}</h3>
+        <button>Delete</button>
       </div>
-      <button>Delete</button>
     </>
   );
 }
