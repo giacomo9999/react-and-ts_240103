883d85627f2e6e927e2fd0e13f0c913f360d7f90
giacomo9999 <gmarchesi888@gmail.com>
2024-01-09 15:14:29 -0500

    Back to where I was the other day.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+
 import Book from "./components/Book";
+import Header from "./components/Header";
+import goalsImg from "./assets/goals.jpg";
+
+type BookProps = {
+  title: string;
+  author: string;
+  id: number;
+};
 
 export default function App() {
+  const [books, setBooks] = useState<BookProps[]>([]);
+
+  function handleAddBook() {
+    setBooks((prevBooks) => {
+      const newBook: BookProps = {
+        id: Math.random(),
+        title: "New Book",
+        author: "Gern Blanston",
+      };
+      return [...prevBooks, newBook];
+    });
+  }
+
   return (
     <main className="container-outer">
-      <Book title="The Dying Earth" author="Jack Vance" />
+      <Header image={{ src: goalsImg, alt: "picture of some books" }}>
+        <h1>Books To Read</h1>
+      </Header>
+      <ul>
+        {books.map((book) => {
+          return (
+            <li>
+              <Book title={book.title} author={book.author} id={book.id} />
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={handleAddBook}>Add Book</button>
     </main>
   );
 }
--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,17 +1,20 @@
-export default function Book({
-  title,
-  author,
-}: {
+import { PropsWithChildren } from "react";
+
+type BookProps = PropsWithChildren<{
   title: string;
   author: string;
-}) {
+  id: number;
+}>;
+
+export default function Book({ title, author, children }: BookProps) {
   return (
     <article>
       <div className="container-inner">
         <h2>{title}</h2>
-        <p>{author}</p>
+        <h3>{author}</h3>
+        <button>Delete</button>
       </div>
-      <button>DELETE</button> 
+      
     </article>
   );
 }
